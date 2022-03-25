b3695d41494b78eb60c79eec8ec08d7c566e2c3e
Eric Honas <honased@gmail.com>
2022-03-24 21:58:49 -0500

    Only render tiles visible in view.

--- HonasGame/Tiled/Entities/TiledTilesEntity.cs
+++ HonasGame/Tiled/Entities/TiledTilesEntity.cs
@@ -40,43 +40,57 @@ namespace HonasGame.Tiled.Entities
             string cachedTexStr = "";
             Texture2D cachedTex = null;
 
-            for (int j = 0; j < Width * Height; j++)
+            int oX = (int)Camera.Position.X / TileWidth - 2;
+            int oY = (int)Camera.Position.Y / TileHeight - 2;
+            oX = MathHelper.Clamp(oX, 0, Width);
+            oY = MathHelper.Clamp(oY, 0, Height);
+
+            int xStop = (int)Camera.CameraSize.X / TileWidth + 4;
+            int yStop = (int)Camera.CameraSize.Y / TileHeight + 4;
+            xStop = MathHelper.Clamp(oX + xStop, 0, Width);
+            yStop = MathHelper.Clamp(oY + yStop, 0, Height);
+
+            for (int y = oY; y < yStop; y++)
             {
-                uint tile = _tiles[j];
-
-                if (tile == 0) continue;
-
-                SpriteEffects effects = SpriteEffects.None;
-                bool flippedVertically = (tile & FLIPPED_VERTICALLY) > 0;
-                bool flippedHorizontally = (tile & FLIPPED_HORIZONTALLY) > 0;
-                tile &= ~(FLIPPED_HORIZONTALLY | FLIPPED_VERTICALLY | FLIPPED_DIAGONALLY | FLIPPED_HEXAGONAL);
-
-                if (flippedHorizontally) effects |= SpriteEffects.FlipHorizontally;
-                if (flippedVertically) effects |= SpriteEffects.FlipVertically;
-
-                // Find Tileset
-                for (int k = _tilesets.Count - 1; k >= 0; k--)
+                for (int x = oX; x < xStop; x++)
                 {
-                    var _set = _tilesets[k];
-                    if (_set.FirstGid <= tile)
+                    int j = (y * Width) + x;
+                    uint tile = _tiles[j];
+
+                    if (tile == 0) continue;
+
+                    SpriteEffects effects = SpriteEffects.None;
+                    bool flippedVertically = (tile & FLIPPED_VERTICALLY) > 0;
+                    bool flippedHorizontally = (tile & FLIPPED_HORIZONTALLY) > 0;
+                    tile &= ~(FLIPPED_HORIZONTALLY | FLIPPED_VERTICALLY | FLIPPED_DIAGONALLY | FLIPPED_HEXAGONAL);
+
+                    if (flippedHorizontally) effects |= SpriteEffects.FlipHorizontally;
+                    if (flippedVertically) effects |= SpriteEffects.FlipVertically;
+
+                    // Find Tileset
+                    for (int k = _tilesets.Count - 1; k >= 0; k--)
                     {
-                        tile -= (uint)_set.FirstGid;
-
-                        TiledTileset actualSet = Assets.AssetLibrary.GetAsset<TiledTileset>(_set.Source);
-
-                        if (cachedTexStr != actualSet.Image)
+                        var _set = _tilesets[k];
+                        if (_set.FirstGid <= tile)
                         {
-                            cachedTexStr = actualSet.Image;
-                            cachedTex = Assets.AssetLibrary.GetAsset<Texture2D>(actualSet.Image);
+                            tile -= (uint)_set.FirstGid;
+
+                            TiledTileset actualSet = Assets.AssetLibrary.GetAsset<TiledTileset>(_set.Source);
+
+                            if (cachedTexStr != actualSet.Image)
+                            {
+                                cachedTexStr = actualSet.Image;
+                                cachedTex = Assets.AssetLibrary.GetAsset<Texture2D>(actualSet.Image);
+                            }
+
+                            Vector2 pos = new Vector2(x * TileWidth, y * TileHeight);
+                            Vector2 sourcePos = actualSet.GetTileLocation(tile);
+                            Rectangle sourceRect = new Rectangle((int)sourcePos.X, (int)sourcePos.Y, TileWidth, TileHeight);
+                            spriteBatch.Draw(cachedTex, pos, sourceRect, Color.White, 0.0f, Vector2.Zero, Vector2.One, effects, 0.0f);
+                            break;
                         }
 
-                        Vector2 pos = new Vector2((j % Width) * TileWidth, (j / Width) * TileHeight);
-                        Vector2 sourcePos = actualSet.GetTileLocation(tile);
-                        Rectangle sourceRect = new Rectangle((int)sourcePos.X, (int)sourcePos.Y, TileWidth, TileHeight);
-                        spriteBatch.Draw(cachedTex, pos, sourceRect, Color.White, 0.0f, Vector2.Zero, Vector2.One, effects, 0.0f);
-                        break;
                     }
-
                 }
             }
 
